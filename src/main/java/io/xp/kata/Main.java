1e2f5e176cb52dfe1efdcbbb94814413432ae09f
Vic Wu <madcocomo@gmail.com>
2021-11-07 11:08:59 +0800

    add example code of read arguments and config file

--- src/main/java/io/xp/kata/Main.java
+++ src/main/java/io/xp/kata/Main.java
@@ -1,6 +1,25 @@
 package io.xp.kata;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
+        String greeting = loadExampleProperties();
+        String name = "developer";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.printf("%s %s", greeting, name);
+    }
+
+    private static String loadExampleProperties() {
+        try {
+            Properties properties = new Properties();
+            properties.load(Main.class.getResourceAsStream("/config.properties"));
+            return properties.getProperty("greeting");
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
